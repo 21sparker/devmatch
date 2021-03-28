63e7f9773c083a36c3d3295f257a0c46a9297cd9
21sparker <seanparker500@gmail.com>
2021-03-28 17:47:44 -0400

    Fix tailwind color issue

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,3 @@
-import colors from 'tailwindcss/colors';
-
 module.exports = {
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
