2bf1530b3f59c7877c9cef2ddceb67d08dc37ccd
changangus <63685366+changangus@users.noreply.github.com>
2021-04-18 11:28:14 -0400

    have some of the questions styled

--- components/Checkbox.tsx
+++ components/Checkbox.tsx
@@ -7,8 +7,8 @@ interface CheckboxProps {
 const Checkbox: React.FC<CheckboxProps> = ({label}) => {
     return (
       <div className="mt-1">
-        <label className="inline-flex items-center">
-          <input type="checkbox" value={label} className="form-checkbox h-8 w-8" />
+        <label className="inline-flex items-center" htmlFor={label}>
+          <input type="checkbox" value={label} name={label} className="form-checkbox h-8 w-8" />
           <span className="ml-4 text-xl">{label}</span>
         </label>
       </div>
--- pages/questionnaire.tsx
+++ pages/questionnaire.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Checkbox from '../components/Checkbox';
 import Header from '../components/Header';
 import Navbar from '../components/Navbar/Navbar';
+import TextInput from '../components/TextInput';
 
 interface questionnaireProps {
 
@@ -12,18 +13,36 @@ const questionnaire: React.FC<questionnaireProps> = ({}) => {
       <>
         <Navbar/>
         <form id="questionnaire-container"
-          className="flex flex-col items-center mt-16">
-          <div id="question flex w-full justify-center">
+          className="flex flex-col mt-16">
+          <div id="question"
+            className="flex w-full">
             <Header size="4xl" center>What languages do you use?</Header>
             <div className="grid grid-cols-4 gap-4 mt-8">
               <Checkbox label="Javascript" />
-              <Checkbox label="Javascript" />
-              <Checkbox label="Javascript" />
-              <Checkbox label="Javascript" />
-              <Checkbox label="Javascript" />
-              <Checkbox label="Javascript" />
-              <Checkbox label="Javascript" />
-              <Checkbox label="Javascript" />
+              <Checkbox label="Python" />
+              <Checkbox label="Java" />
+              <Checkbox label="Ruby" />
+              <Checkbox label="C#" />
+              <Checkbox label="Golang" />
+              <Checkbox label="PHP" />
+              <Checkbox label="C++" />
+            </div>
+          </div>
+          <div id="question"
+            className="flex flex-col mt-24">
+            <Header size="4xl" center>Where do you live?</Header>
+            <div className="mt-8">
+              <TextInput placeholder="San Francisco, CA" name="location"/>
+            </div>
+          </div>
+          <div id="question"
+            className="flex flex-col mt-24 justify-center">
+            <Header size="4xl" center>Describe your experience as a programmer.</Header>
+            <div className="mt-8">
+              <Checkbox label="I've never programmed before." />
+              <Checkbox label="I've built a few small projects." />
+              <Checkbox label="I've built a few full stack projects." />
+              <Checkbox label="I am a professional in the field." />
             </div>
           </div>
         </form>
