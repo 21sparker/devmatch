43a6139b768c518dbe1961d548677fd3b7572554
21sparker <seanparker500@gmail.com>
2021-04-18 10:19:48 -0400

    Add temp home icon

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -1,15 +1,16 @@
 import React, { useState } from 'react';
 import Container from './Container';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faPeopleArrows } from '@fortawesome/free-solid-svg-icons'
 
-const linkClassName = "inline-block p-3 text-lg";
-
+const linkClassName = "inline-block p-3 text-xl font-semibold";
 
 const Navbar: React.FC = () => {
   return (
         <Container>
             <div id="left-nav">
                 <ul>
-                    <li className={linkClassName}><a href="#">Icon</a></li>
+                    <li className={linkClassName}><a href="#"><FontAwesomeIcon icon={faPeopleArrows} /></a></li>
                 </ul>            
             </div>
             <div id="right-nav" className="grid justify-items-stretch">
